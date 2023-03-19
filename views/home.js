a5a60a1a3d50d852f52fbbc29e229d2089d7fde4
Jessica Arambula <63938881+jessica0696@users.noreply.github.com>
2023-03-19 15:03:33 -0500

    Update home.js

--- views/home.js
+++ views/home.js
@@ -4,8 +4,11 @@ const router = express.Router();
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
     title: "Express Testing",
-    message: "The app is working properly!",
+    message: "The app is working properly! ",
+
   });
 });
-
+<li class="nav-item">
+<a class="nav-link" href="./mapa/mapa.html">mapa</a>
+</li>
 module.exports = router;
