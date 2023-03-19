94b2f918eeb1f0a49443410ac8641b5d7aa29754
Jessica Arambula <63938881+jessica0696@users.noreply.github.com>
2023-03-19 14:25:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 // Import packages
 const express = require("express");
-const session = require('express-session');
-const passport = require('passport');
-const passportLocal = require('passport-local');
-const path = require("path");
-const bodyParser = require('body-parser');
-const sqlite3 = require("sqlite3").verbose();
 const home = require("./views/home");
 
 // Middlewares
 const app = express();
 app.use(express.json());
-// Configuración del servidor
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use("/home", home);
-
-//login procedure
-app.post("/login", passport.authenticate("local", { failureRedirect: "/login" }),
-  (req, res) => { res.redirect("/prin"); });
-
 
 // connection
 const port = process.env.PORT || 9001;
