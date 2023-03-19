cc9c562200103fed55d8334d5366e7acc3726a41
Jessica Arambula <63938881+jessica0696@users.noreply.github.com>
2023-03-19 14:20:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const passportLocal = require('passport-local');
 const path = require("path");
 const bodyParser = require('body-parser');
 const sqlite3 = require("sqlite3").verbose();
-const home = require("./views/register");
+const home = require("./views/home");
 
 // Middlewares
 const app = express();
@@ -14,9 +14,7 @@ app.use(express.json());
 
 // Routes
 app.use("/home", home);
-app.get("/register", (req, res) => {
-    res.render("signup");
-  });
+
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
