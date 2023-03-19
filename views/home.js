f0ed7a1bb280b132e245495450ce3d1503801108
Jessica Arambula <63938881+jessica0696@users.noreply.github.com>
2023-03-19 14:45:08 -0500

    Update home.js

--- views/home.js
+++ views/home.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-app.get("/login", (req, res) => {
-    res.render("login");
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "asi si funciona",
+    message: "hola mundo",
   });
+});
 
 module.exports = router;
