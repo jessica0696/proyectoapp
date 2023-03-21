ac0f4c678e3b7e9d75acf327ec8eb0ae07582e88
Jessica Arambula <63938881+jessica0696@users.noreply.github.com>
2023-03-21 17:46:38 -0500

    Update mapa.js

--- public/css/mapa.js
+++ public/css/mapa.js
@@ -3,12 +3,10 @@ var map = L.map('map').setView([20.68139306044883, -103.35294671539657], 20);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
+document.getElementById('select-location').addEventListener('change',function(e){
+    let coords = e.target.value.split(",");
+    map.flyTo(coords,15);
+  });
 L.marker([20.68139306044883, -103.35294671539657]).addTo(map)
     .bindPopup('Carro abandonado')
     .openPopup();
-
-    document.getElementById('select-location').addEventListener('change',function(e){
-        let coords = e.target.value.split(",");
-        map.flyTo(coords,13);
-      })
