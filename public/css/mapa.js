a879fa946f452a42892bab5e8128b858e3958230
Jessica Arambula <63938881+jessica0696@users.noreply.github.com>
2023-03-21 17:17:33 -0500

    Update mapa.js

--- public/css/mapa.js
+++ public/css/mapa.js
@@ -5,5 +5,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .bindPopup('Echale OJO')
     .openPopup();
