734339608c85a9436c16870134532be88cfb8ccf
Jessica Arambula <63938881+jessica0696@users.noreply.github.com>
2023-03-22 10:57:29 -0500

    flecha

--- public/css/mapa.js
+++ public/css/mapa.js
@@ -21,3 +21,23 @@ L.Control.geocoder().addTo(map);
 map.on("click", function(e){
     var marker = new L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
 });
+
+
+//flecha
+$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
+});
