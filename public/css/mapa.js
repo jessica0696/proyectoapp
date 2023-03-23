f76b10aa49cac3d844fad54c0e207499912c10b3
Jessica Arambula <63938881+jessica0696@users.noreply.github.com>
2023-03-23 12:37:36 -0500

    Update mapa.js

--- public/css/mapa.js
+++ public/css/mapa.js
@@ -1,3 +1,20 @@
+//flecha up 
+$(document).ready(function(){
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+});
+
 //mapa leaflet
 var map = L.map('map').setView([20.68139306044883, -103.35294671539657], 13);
 
@@ -40,19 +57,3 @@ const getData = async () =>{
 
 getData();
 
-//flecha up 
-$(document).ready(function(){
-	$('.ir-arriba').click(function(){
-		$('body, html').animate({
-			scrollTop: '0px'
-		}, 300);
-	});
-
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
-			$('.ir-arriba').slideDown(300);
-		} else {
-			$('.ir-arriba').slideUp(300);
-		}
-	});
-});
